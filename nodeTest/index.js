c6f00b19bfe518f626e6eb9dca207a0a2f59bcbf
alexnortn <alexnortn@gmail.com>
2014-12-23 00:51:52 -0500

    physicsjs

--- nodeTest/index.js
+++ nodeTest/index.js
@@ -1,3 +1,4 @@
 var server = require("./server");
+var	router = require("./router");
 
-server.begin();
+server.begin(router.route);
--- nodeTest/server.js
+++ nodeTest/server.js
@@ -2,10 +2,15 @@
 // Alex Norton 2014
 
 var http = require("http");
+var url = require("url");
 
-function begin() {
+function begin(route) {
   function onRequest(request, response) {
-    console.log("Request received.");
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+    route(pathname);
+
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
