e5462da81a73e07f1a414a2b9cfc71ab57a736c6
alexnortn <alexnortn@gmail.com>
2015-04-19 17:32:03 -0400

    made it to a good place

--- swarming/Assets/Scripts/GameController.cs
+++ swarming/Assets/Scripts/GameController.cs
@@ -1,18 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 
 	public GameObject[] thoughts;
 	public Vector3 spawnValues;
+	public Vector2 spawnScaleRange;
+	public List<GameObject> thoughtList;
+	public AudioClip[] audioList;
 	public int thoughtCount;
 	public float spawnWait;
 	public float startWait;
 	public float waveWait;
 
+	private int spawnScale;
+
 	// Use this for initialization
 	void Start () {
+		thoughtList = new List<GameObject>();
+		Physics.gravity = new Vector3(0, -0.75F, 0);
 		StartCoroutine (SpawnWaves ());	
+
+		//Loading the audio items into the array
+        audioList =  new AudioClip[]
+        {
+        	Resources.Load("Sounds/thoughtSound1") as AudioClip,
+        	Resources.Load("Sounds/thoughtSound2") as AudioClip,
+        	Resources.Load("Sounds/thoughtSound3") as AudioClip,
+        	Resources.Load("Sounds/thoughtSound4") as AudioClip,
+        	Resources.Load("Sounds/thoughtSound5") as AudioClip,
+        	Resources.Load("Sounds/thoughtSound6") as AudioClip,
+        	Resources.Load("Sounds/thoughtSound7") as AudioClip,
+        	Resources.Load("Sounds/thoughtSound8") as AudioClip,
+        	Resources.Load("Sounds/thoughtSound9") as AudioClip,
+        	Resources.Load("Sounds/thoughtSound10") as AudioClip,
+        	Resources.Load("Sounds/thoughtSound11") as AudioClip
+        	
+        };
 	}
 	
 	// Update is called once per frame
@@ -22,15 +47,30 @@ public class GameController : MonoBehaviour {
 
 	IEnumerator SpawnWaves ()
 	{
+		GameObject thought;
 		yield return new WaitForSeconds (startWait);
 		while (true)
 		{
 			for (int i = 0; i < thoughtCount; i++)
 			{
-				GameObject thought = thoughts [Random.Range (0, thoughts.Length)];
-				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				// thought = thoughts [Random.Range (0, thoughts.Length)];
+
+				spawnScale = Mathf.RoundToInt(Random.Range(spawnScaleRange.x, spawnScaleRange.y));
+
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, Random.Range (-spawnValues.z, spawnValues.z));
 				Quaternion spawnRotation = Quaternion.identity;
-				Instantiate (thought, spawnPosition, spawnRotation);
+				// Instantiate (thought, spawnPosition, spawnRotation);
+
+				thought = (GameObject) GameObject.Instantiate(thoughts [Random.Range (0, thoughts.Length)], spawnPosition, spawnRotation);
+				thought.transform.localScale *= spawnScale;
+				// thought.AddComponent(AudioSource);
+				thought.audio.clip = audioList[Random.Range(0,11)];
+				thought.audio.loop = true;
+				thought.audio.volume = Random.Range(0F, 0.5F);
+				thought.audio.Play();
+
+				thoughtList.Add(thought);
+
 				yield return new WaitForSeconds (spawnWait);
 			}
 			yield return new WaitForSeconds (waveWait);
